105e5491bd59794560bd4dd8c36b9e383a094541
David Torres <davidbenjtorres@gmail.com>
2021-11-30 18:59:02 -0700

    Added several customizable settings to the settings page

--- RubberDuck/venv/Scripts/main.py
+++ RubberDuck/venv/Scripts/main.py
@@ -12,7 +12,12 @@ from kivy.uix.anchorlayout import AnchorLayout
 from kivy.properties import NumericProperty
 from kivy.core.audio import SoundLoader
 from kivy.clock import Clock
+from win32api import GetSystemMetrics
 import time
+
+global alarmActive, quackActive
+alarmActive = True
+quackActive = True
 
 class MainWindow(Screen):
     pass
@@ -22,28 +27,66 @@ class SecondWindow(Screen):
 
     pass
 
+class SettingsWindow(Screen):
+    pass
+
 class WindowManager(ScreenManager):
     pass
 
 class MyApp(App):
     def build(self):
-        self.playSound_quack()
-        self.title = 'Rubber Duck'
+        Window.borderless = True
         Window.clearcolor = (5/255, 45/255, 60/255, 1)    # app background color
         Window.size = (500, 400)                          # size of entire app window
-        return Builder.load_file("./../Kivy/my.kv")
+        Window.top = (GetSystemMetrics(1)/2) - 225
+        Window.left = (GetSystemMetrics(0)/2) - 250
+
+        self.playSound_quack()
+
+        return Builder.load_file("../Kivy/my.kv")
+
+    def alarm_switch(self, switchObject, switchValue, labelId):
+        if(switchValue):
+            labelId.text = "Alarm Enabled"
+            alarmActive = True
+        else:
+            labelId.text = "Alarm Disabled"
+            alarmActive = False
+
+    def quack_switch(self, switchObject, switchValue, labelId):
+        if(switchValue):
+            labelId.text = "Quacks Enabled"
+            quackActive = True
+        else:
+            labelId.text = "Quacks Disabled"
+            quackActive = False
+
+    def closeApp_pressed(self, id):
+        id.source = "../Assets/close_pressed.png"
+
+    def closeApp_released(self, id):
+        id.source = "../Assets/close.png"
+        quit()
+
+    def options_pressed(self, id):
+        id.source = "../Assets/gear_pressed.png"
+
+    def options_released(self, id):
+        id.source = "../Assets/gear.png"
 
     # alarm sound effect
     def playSound_alarm(self):
-        sound = SoundLoader.load("../Assets/alarmclock_sound.wav")
-        if sound:
-            sound.play()
+        if alarmActive is True:
+            sound = SoundLoader.load("../Assets/alarmclock_sound.wav")
+            if sound:
+                sound.play()
 
     # quack sound effect
     def playSound_quack(self):
-        sound = SoundLoader.load("../Assets/duckquack_sound.wav")
-        if sound:
-            sound.play()
+        if quackActive is True:
+            sound = SoundLoader.load("../Assets/duckquack_sound.wav")
+            if sound:
+                sound.play()
 
     # behavior for 'break' action
     def timedBreak(self, time):
