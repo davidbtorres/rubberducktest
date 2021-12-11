9bcb9d289561edc96ebcedd780f1f68bca29e618
David Torres <davidbenjtorres@gmail.com>
2021-12-10 19:49:25 -0700

    Numerous changes

--- RubberDuck/venv/Scripts/main.py
+++ RubberDuck/venv/Scripts/main.py
@@ -9,17 +9,13 @@ from kivy.uix.image import Image
 from kivy.uix.textinput import TextInput
 from kivy.core.window import Window
 from kivy.uix.anchorlayout import AnchorLayout
-from kivy.properties import NumericProperty, StringProperty, BooleanProperty
+from kivy.properties import NumericProperty, StringProperty, BooleanProperty, ObjectProperty
 from kivy.core.audio import SoundLoader
 from kivy.clock import Clock
 from win32api import GetSystemMetrics
-import datetime
+from datetime import datetime, timedelta
 import sys
 import time
-
-#global alarmActive, quackActive
-#alarmActive = True
-#quackActive = True
 
 class StartUpWindow(Screen):
     pass
@@ -48,22 +44,35 @@ class MyApp(App):
         Window.top = (GetSystemMetrics(1)/2) - 225
         Window.left = (GetSystemMetrics(0)/2) - 250
 
+        self.startTime = datetime.now()
+
+        # default settings
         self.alarmActive = True
         self.quackActive = True
         self.checkBackTime = 15
+        self.devModeMultiplier = 60
 
+        # app quacks on launch
         self.playSound_quack()
 
         return Builder.load_file("../Kivy/my.kv")
 
-    #def startCountdown(self, time):
-    #    self.function_interval = Clock.schedule_interval(self.updateCountdown, 1)
+    def updateTimeWorked(self, label):
+        timeWorked = str(datetime.now() - self.startTime)
+        hours = timeWorked[0:1]
+        if timeWorked[2:3] == "0":
+            minutes = timeWorked[3:4]
+        else:
+            minutes = timeWorked[2:4]
 
-    #def stopCountdown(self, *args):
-    #    self.function_interval.cancel()
-
-    #def updateCountdown(self, *args):
-    #    label.text = str(int(label.text) - 1)
+        if hours != "1" and minutes != "1":
+            label.text = "Just Checking in!\nIt has been a while since your last break\n\n\nTotal Time Worked:\n" + hours + " hours and " + minutes + " minutes"
+        elif hours != "1" and minutes == "1":
+            label.text = "Just Checking in!\nIt has been a while since your last break\n\n\nTotal Time Worked:\n" + hours + " hours and " + minutes + " minute"
+        elif hours == "1" and minutes != "1":
+            label.text = "Just Checking in!\nIt has been a while since your last break\n\n\nTotal Time Worked:\n" + hours + " hour and " + minutes + " minutes"
+        elif hours == "1" and minutes == "1":
+            label.text = "Just Checking in!\nIt has been a while since your last break\n\n\nTotal Time Worked:\n" + hours + " hour and " + minutes + " minute"
 
     def alarm_switch(self, switchObject, switchValue, labelId):
         if(switchValue):
@@ -80,6 +89,14 @@ class MyApp(App):
         else:
             labelId.text = "Quacks Disabled"
             self.quackActive = False
+
+    def dev_switch(self, switchObject, switchValue, labelId):
+        if (switchValue):
+            labelId.text = "Developer Mode Enabled"
+            self.devModeMultiplier = 1
+        else:
+            labelId.text = "Developer Mode Disabled"
+            self.devModeMultiplier = 60
 
     def closeApp_pressed(self, id):
         id.source = "../Assets/close_pressed.png"
@@ -108,31 +125,69 @@ class MyApp(App):
             if sound:
                 sound.play()
 
+    def initTimeLeftLabel(self, labelId):
+        global timeLeftLabel
+        timeLeftLabel = labelId
+
     # behavior for 'break' action
     def timedBreak(self, time):
-        #self.greeting.text = "See you in 30 minutes!"
-        #App.get_running_app().root_window.minimize()
-        Clock.schedule_once(self.breakOver, time) # time reduced to seconds for testing
-        #self.startCountdown(time)
+        time = time * self.devModeMultiplier
+        #Clock.schedule_once(self.breakOver, time) # time reduced to seconds for testing
+        #self.root.BreakWindow.startCountdown(time)
+        h = 0 #time // 60
+        m = time // 60
+        s = time % 60
+        #print(h)
+        #print(m)
+        #print(s)
+        self.delta = datetime.now() + timedelta(hours=h, minutes=m, seconds = s)
+        self.function_interval = Clock.schedule_interval(self.updateCount, 1)
+
+    def updateCount(self, *args):
+        global timeLeftLabel
+        timeLeft = str(self.delta - datetime.now())
+        print(timeLeft[0:7])
+        timeLeftLabel.text = "Break Time Remaining:\n" + timeLeft[0:7]
+        if timeLeft[0:7] == "0:00:00":
+            self.stopCount()
+            self.breakOver()
+        elif timeLeft[0:7] == "-1 day,":
+            self.stopCount()
+            self.breakOver()
+
+    def stopCount(self, *args):
+        self.function_interval.cancel()
+
+    def dismissForBreak(self):
+        App.get_running_app().root_window.minimize()
+
+    def changeBackgroundColor(self, instance):
+        if Window.clearcolor == (255 / 255, 255 / 255, 255 / 255, 1):
+            Window.clearcolor = (255 / 255, 0 / 255, 0 / 255, 1)
+        if Window.clearcolor == (255 / 255, 0 / 255, 0 / 255, 1):
+            Window.clearcolor = (255 / 255, 255 / 255, 255 / 255, 1)
 
     # restoring app on break finish
-    def breakOver(self, instance):
+    def breakOver(self):
         self.playSound_quack()
         self.playSound_alarm()
         App.get_running_app().root_window.restore()
-        self.root.current = "main"
-        #self.greeting.text = "Welcome back!"
+        self.root.current = "startup"
+
+    # ending break from 'end break now' button
+    def forceBreakOver(self):
+        self.stopCount()
+        self.root.current = "startup"
 
     # 'dismiss' handling
     def checkBackLater(self):
-        Clock.schedule_once(self.checkIn, 5) # time set to 5 seconds for testing and demoing
+        Clock.schedule_once(self.checkIn, self.checkBackTime * self.devModeMultiplier) # time set to 5 seconds for testing and demoing
         App.get_running_app().root_window.minimize()
 
     # 'check in' handling
     def checkIn(self, instance):
         self.playSound_quack()
         App.get_running_app().root_window.restore()
-        #self.greeting.text = "Quack!\n\nChecking in!\nYou Have Been Working For a While Now"
 
 if __name__ == "__main__":
     MyApp().run()
